8a4882b1597b8d5b89f8695204c6171c23d5926e
Milagros Cordova <m.cordova.33@gmail.com>
2021-11-02 15:49:04 -0400

    Routes Controller pt-1

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :bookings
-  belongs_to :route
+  has_one :route
 
 
   devise :database_authenticatable, :registerable,
