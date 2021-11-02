675bbba25dcbd5999ec9bdf1a48e4fd07da1d448
Milagros Cordova <m.cordova.33@gmail.com>
2021-11-02 16:41:03 -0400

    View index - routes

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,5 +3,5 @@ class Route < ApplicationRecord
   has_many :bookings
   belongs_to :user, optional: true
   has_many :places, through: :connection
-  validates :checkin, :duration, :description, :available_dates, :name, presence: true
+  validates :places_interest, :duration, :description, :available_dates, :name, presence: true
 end
