da71fe1a8592a52a1efe8fc0a64c83a72e2298e8
katherine colina <kathymcolina@gmail.com>
2021-11-05 08:23:28 -0400

    test markers map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -27,17 +27,6 @@ const initMapbox = () => {
     });
 
     fitMapToMarkers(map, markers);
-
-    const addMarkersToMap = (map, markers) => {
-      markers.forEach((marker) => {
-        const popup = new mapboxgl.Popup().setHTML(marker.info_window); // add this
-
-        new mapboxgl.Marker()
-          .setLngLat([marker.lng, marker.lat])
-          .setPopup(popup) // add this
-          .addTo(map);
-      });
-    };
   }
 };
 
