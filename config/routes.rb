5a0a1de806aa2eb95d2c55a654a6c2a6133cf296
luiszambrano <luisdavidzambrano@gmail.com>
2021-11-08 11:08:59 -0400

    pago_listo_front

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
     resources :reviews, only: [:create, :new]
   end
   resources :places
-  resources :bookings, only: :index
+  resources :bookings, only: [:index, :edit, :update]
 end
